58cd93e814b88f388feff6eef563a3a7e3b007b9
James de Jesus <james.dejesus@clorox.com>
2021-01-29 16:16:19 -0800

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,39 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import data from "../data/config.json"
 
 export default function Home() {
+
+  // const handleClick = api => e => {
+  //   e.preventDefault()
+
+  //   this.setState({ loading: true })
+  //   fetch("/.netlify/functions/" + api)
+  //     .then(response => response.json())
+  //     .then(json => this.setState({ loading: false, msg: json.msg }))
+  // }
+
+  const handleClick = (e) => {
+    console.log('handleClick: ', e.target.innerHTML);
+       fetch("/.netlify/functions/triggerTest")
+      .then(response => response.json())
+      .then(json => this.setState({ loading: false, msg: json.msg }))
+  }
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>QA Automation Dashboard</title>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      <header  className="flex justify-center">
+        <h1>QA Automation Dashboard</h1>
+      </header>
+      <section className="grid grid-cols-3 gap-4">
+        {
+            data.map( site => (
+                <button onClick={handleClick} className="rounded-lg" key={site.key}>{site.name}</button>
+            ))
+        }
+      </section>
+    </>
   )
 }
