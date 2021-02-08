b97db2eb5473482177f182c174be1f9c0f2d5169
James de Jesus <james.dejesus@clorox.com>
2021-02-08 15:53:52 -0800

    try org restriction

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,7 +6,8 @@ export default NextAuth({
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
+      clientSecret: process.env.GITHUB_SECRET,
+      allowOrgs: "electro-create-workshop"
     }),
   ],
 
