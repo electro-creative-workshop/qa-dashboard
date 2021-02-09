3943b36e10e17743725c077c6eb3c2b3484c326b
James de Jesus <james.dejesus@clorox.com>
2021-02-08 16:50:41 -0800

    trying Auth0

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,11 +4,16 @@ import Providers from 'next-auth/providers'
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-      allowOrgs: "electro-create-workshop"
-    }),
+    // Providers.GitHub({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    //   allowOrgs: "electro-create-workshop"
+    // }),
+    Providers.Auth0({
+        clientId: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        domain: process.env.AUTH0_DOMAIN
+      })
   ],
 
   // A database is optional, but required to persist accounts in a database
